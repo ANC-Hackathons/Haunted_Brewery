9c09c5b0bac1f8c19f965f06fce273c42c12645c
Nathan Thompson <nathan.thompson@readytalk.com>
2016-06-10 21:01:24 -0600

    Get API working; init web page

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var request = require('superagent');
 var prefix = require('superagent-prefix')('http://apis.mondorobot.com');
 
+var express = require('express');
+var app = express();
+
 request
-  .get('/beers')
+  .get('/beers/ellie-s-brown-ale')
   .use(prefix)
-  .end(funciton(err, res) {
-    console.log(res);
+  .end(function(err, res) {
+    console.log(res.body);
   });
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
