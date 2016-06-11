51e37d2745f60d13073fc41b0588c588946e1759
John Kearns <kearnsiii@gmail.com>
2016-06-11 11:03:23 -0600

    updated API functions

--- index.js
+++ index.js
@@ -91,6 +91,156 @@ request
   .get('/taproom/menu')
   .use(prefix)
   .end(function(err, response) {
-    console.log(response.body.menu);
+    //console.log(response.body.menu);
     food = response.body.menu;
   });
+
+var current_beers = [];
+var current_beers_relevant = [];
+
+request
+  .get('/taproom/on-tap')
+  .use(prefix)
+  .end(function(err, response) {
+    //console.log(response.body.beer_list.beers);
+    current_beers = response.body.beer_list.beers;
+    current_beers_relevant = current_beers.map(function(i){
+      return {
+            name: i.name,
+            abv: i.abv,
+            img: i.label_image.original
+      }
+    });
+  });
+
+app.get('/current-beers', function(request, response) {
+      response.send(
+        current_beers_relevant
+      );
+});
+
+var hog_heaven = {};
+request
+  .get('/beers/hog-heaven')
+  .use(prefix)
+  .end(function(err, response) {
+    hog_heaven = response;
+  });
+
+app.get('/hog-heaven', function(request, response) {
+      response.send(
+        hog_heaven
+      );
+});
+
+var the_reverend = {};
+request
+  .get('/beers/the-reverend')
+  .use(prefix)
+  .end(function(err, response) {
+    the_reverend = response;
+  });
+
+app.get('/the-reverend', function(request, response) {
+      response.send(
+        the_reverend
+      );
+});
+
+var salvation = {};
+request
+  .get('/beers/salvation')
+  .use(prefix)
+  .end(function(err, response) {
+    salvation = response;
+  });
+
+app.get('/salvation', function(request, response) {
+      response.send(
+        salvation
+      );
+});
+
+var the_maharaja = {};
+request
+  .get('/beers/the-maharaja')
+  .use(prefix)
+  .end(function(err, response) {
+    the_maharaja = response;
+  });
+
+app.get('/the-maharaja', function(request, response) {
+      response.send(
+        the_maharaja
+      );
+});
+
+var the_kaiser = {};
+request
+  .get('/beers/the-kaiser')
+  .use(prefix)
+  .end(function(err, response) {
+    the_kaiser = response;
+  });
+
+app.get('/the-kaiser', function(request, response) {
+      response.send(
+        the_kaiser
+      );
+});
+
+var the_czar = {};
+request
+  .get('/beers/the-czar')
+  .use(prefix)
+  .end(function(err, response) {
+    the_czar = response;
+  });
+
+app.get('/the-czar', function(request, response) {
+      response.send(
+        the_czar
+      );
+});
+
+var mephistopheles = {};
+request
+  .get('/beers/mephistopheles')
+  .use(prefix)
+  .end(function(err, response) {
+    mephistopheles = response;
+  });
+
+app.get('/mephistopheles', function(request, response) {
+      response.send(
+        mephistopheles
+      );
+});
+
+var samael = {};
+request
+  .get('/beers/samael-s')
+  .use(prefix)
+  .end(function(err, response) {
+    samael = response;
+  });
+
+app.get('/samael', function(request, response) {
+      response.send(
+        samael
+      );
+});
+
+var the_beast = {};
+request
+  .get('/beers/the-beast')
+  .use(prefix)
+  .end(function(err, response) {
+    the_beast = response;
+  });
+
+app.get('/the-beast', function(request, response) {
+      response.send(
+        the_beast
+      );
+});
